e94875eae5fd322cc380d86c22548af3b73f44c2
dragsec <draganyes@gmail.com>
2021-04-22 09:01:08 +0200

    mmm

--- js/liveScript.js
+++ js/liveScript.js
@@ -1,9 +1,23 @@
+/* inizio evento*/
 var buttonPrice = document.getElementById("button");
 
 buttonPrice.addEventListener("click", function (){
 
+    var totPrezzo = 0;
+
+    /* setto prezzo in base al prodotto selezionato */
+    var nameBurger = ["hamburger", "cheeseburger", "superburger"]
+
+    var userBurger = document.getElementById("name");
+
+    for (var i = 0; i < nameBurger.length; i++){
+
+        var nameLocal = nameBurger[i];
+
+        if (nameLocal === userBurger.value){
+            totPrezzo += i;
+        }
+    }
 
 
-
-    
 })
